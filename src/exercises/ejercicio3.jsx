714eb58b24dc7673bc31f10586add7bfa153b238
AnthonyBrito-dev <anthonyrafaelbritobravo10@gmail.com>
2026-03-25 10:46:15 -0500

    Resolucion de ejercicios 3 y 4 de React

--- src/exercises/ejercicio3.jsx
+++ src/exercises/ejercicio3.jsx
@@ -1,10 +1,48 @@
 import Encabezado from "../components/encabezado";
+import { useState } from "react";
 
-export default function Ejercicio3() {
-    const ejercicio3 = ``
+const CampoDeTexto = ()=>{
+    const [listado, setListado] = useState([])
+    const [item, setItem] = useState("")
+
+    const inputChange = (e)=>{
+        setItem(e.target.value)
+    }
+
+    const addText = ()=>{
+        setListado([...listado, item])
+    }
+
+    const removeText = (index)=>{
+        listado.splice(index, 1)
+        setListado([...listado])
+    }
+
     return (
         <>
-        <Encabezado texto={ejercicio3} num={3}></Encabezado>
+        <input onChange={inputChange} className="min-w-100 max-w-200 border-2 border-black mr-4 p-2" type="text" id="2" placeholder="Escribe aqui.."/>
+        <button onClick={addText} className="p-2 border-2 border-black hover:cursor-pointer">Agregar</button>
+        <ul>
+            {listado.map((lista, i)=>
+                <li className="mb-2" key={i}>{lista} <button className="border-2 border-black p-1 hover:cursor-pointer" onClick={()=>removeText(i)}>X</button></li>
+            )}
+        </ul>
         </>
+    )
+
+}
+
+export default function Ejercicio3() {
+    const eje = 'Crea una página con un campo de texto, un botón que diga "Agregar", y una lista vacía debajo.'
+
+    return (
+        <div className="p-8">
+            <Encabezado texto={eje} num={3}></Encabezado>
+            <ul className="*:list-disc mb-8">
+                <li className="ml-8">Cuando el usuario escriba un texto y haga clic en "Agregar", el texto debe añadirse como un nuevo elemento de la lista.</li>
+                <li className="ml-8">Añade un botón al lado de cada elemento para eliminarlo de la lista.</li>
+            </ul>
+            <CampoDeTexto></CampoDeTexto>
+        </div>
     );
 }
--- src/exercises/ejercicio4.jsx
+++ src/exercises/ejercicio4.jsx
@@ -1,10 +1,43 @@
 import Encabezado from "../components/encabezado";
+import { useState } from "react";
 
-export default function Ejercicio4() {
-    const ejercicio4 = ``
+//Lista de elementos 
+const comidas = ['pizza', 'sushi', 'arepa', 'paella', 'hamburguesa', 'hotdog', ]
+
+function VerComidas(){
+    const [busqueda, setBusqueda] = useState('')
+    
+    //ver el estado del campo de texto
+    const inputChange = (e)=>{
+        setBusqueda(e.target.value)
+    }
+
     return (
         <>
-        <Encabezado texto={ejercicio4} num={4}></Encabezado>
+        <input onChange={inputChange} className="min-w-100 max-w-200 border-2 border-black mb-4 p-2" type="text" id="2" placeholder="Buscar.."/>
+        
+        <ul>
+            {comidas.map((comida, i)=>{
+                if (comida.includes(busqueda.toLowerCase())){  //buscar coincidencias entre letras y mostrar en pantalla
+                    return <li key={i} className="mb-1 ml-5 list-disc">{comida}</li>
+                }
+            })}
+        </ul>
         </>
+    )
+}
+
+export default function Ejercicio4() {
+    const ejercicio4 = `Crea una página con un campo de texto y una lista predefinida de elementos.
+    Mientras el usuario escribe en el campo, la lista debe actualizarse en tiempo real para
+    mostrar solo los elementos que contienen el texto escrito.
+    Ejemplo: Si la lista contiene ["Perro", "Gato", "Pez"] y el usuario escribe "Ga", solo "Gato" debe
+    quedar visible.`
+    
+    return (
+        <div className="p-8">
+        <Encabezado texto={ejercicio4} num={4}></Encabezado>
+        <VerComidas></VerComidas>
+        </div>
     );
 }
