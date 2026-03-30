28593d4367ff0325fd23de8b11f79ab3f7d7ab5d
AnthonyBrito-dev <anthonyrafaelbritobravo10@gmail.com>
2026-03-30 13:56:51 -0500

    Resolucion primera parte de ejercicio 6 react

--- src/exercises/ejercicio6.jsx
+++ src/exercises/ejercicio6.jsx
@@ -1,4 +1,56 @@
 import Encabezado from "../components/encabezado";
+import { useState } from "react";
+
+//Crear el formato del temporizador
+function Temporizador(){
+    const [tiempo, setTiempo] = useState('00:00:00')
+    let seg = 0
+    let min = 0
+    let hr = 0
+    
+    //Funcion para dar formato al tiempo
+    const shownumbers = (num)=>{
+        let numeroFormateado = ``
+
+        if (num < 10){
+            numeroFormateado = `0${num}`
+        } else {
+            numeroFormateado = `${num}`
+        }
+
+        return numeroFormateado
+    }
+
+    //Crear componente iniciar
+    const iniciar = ()=>{
+        setInterval(() => {
+            seg += 1
+            setTiempo(`${shownumbers(hr)}:${shownumbers(min)}:${shownumbers(seg)}`)
+    
+            if (seg == 59){
+                min += 1
+                seg = -1 
+            }
+    
+            if (min > 59){
+                min = 0
+                hr += 1
+            }
+        }, 1000)
+    }
+
+    return (
+        <>
+        <time className="block mb-4" dateTime="HH:MM:SS">{tiempo}</time>
+        <button onClick={iniciar} className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Iniciar</button>
+        <button className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Pausar</button>
+        <button className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Reiniciar</button>
+        </>
+    )
+}
+//Modificar su estado en cada intervalo de un minuto
+//Crear componente pausar
+//Crear componente reiniciar
 
 export default function Ejercicio6() {
     const ejercicio6 = `Crea una página con un temporizador que comience en 00:00:00. Incluye tres botones: “Iniciar”, “Pausar” y “Reiniciar”. 
@@ -7,8 +59,9 @@ export default function Ejercicio6() {
     “Reiniciar” pone el temporizador en 00:00:00`
     
     return (
-        <>
+        <div className="p-8">
         <Encabezado texto={ejercicio6} num={6}></Encabezado>
-        </>
+        <Temporizador></Temporizador>
+        </div>
     );
 }
