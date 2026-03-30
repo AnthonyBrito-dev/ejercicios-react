74012cdab0091f2695cb599b37bbcf0da8cf74c7
AnthonyBrito-dev <anthonyrafaelbritobravo10@gmail.com>
2026-03-30 10:31:33 -0500

    Resolucion del ejercicio 5 React

--- src/exercises/ejercicio5.jsx
+++ src/exercises/ejercicio5.jsx
@@ -1,10 +1,77 @@
 import Encabezado from "../components/encabezado";
+import { useState } from "react";
 
-export default function Ejercicio5() {
-    const ejercicio5 = ``
+function OperacionesMatematicas(){
+    const [primero, setPrimero] = useState(0)
+    const [segundo, setSegundo] = useState(0)
+    const [result, setResult] = useState(0)
+
+    const inputChangeFirst = (e)=>{setPrimero(e.target.value)}
+    const inputChangeSecond = (e)=>{setSegundo(e.target.value)}
+
+    const sumar = ()=>{
+        if (isNaN(parseFloat(primero)) || isNaN(parseFloat(segundo))){
+            setResult("Ingresa un numero valido")
+        } else {
+            setResult(parseFloat(primero) + parseFloat(segundo))
+        }
+    }
+    
+    const restar = ()=>{
+        if (isNaN(parseFloat(primero)) || isNaN(parseFloat(segundo))){
+            setResult("Ingresa un numero valido")
+        } else {
+            setResult(parseFloat(primero) - parseFloat(segundo))
+        }
+    }
+    
+    const multiplicar = ()=>{
+        if (isNaN(parseFloat(primero)) || isNaN(parseFloat(segundo))){
+            setResult("Ingresa un numero valido")
+        } else {
+            setResult(parseFloat(primero) * parseFloat(segundo))
+        }
+    }
+    
+    const dividir = ()=>{
+        if (isNaN(parseFloat(primero)) || isNaN(parseFloat(segundo))){
+            setResult("Ingresa un numero valido")
+        } else if (parseFloat(segundo) === 0) {
+            setResult('No puedes dividir por cero')
+        } else {
+            setResult(parseFloat(primero) / parseFloat(segundo))
+        }
+    }
+    
     return (
         <>
-        <Encabezado texto={ejercicio5} num={5}></Encabezado>
+        <section className="mb-4">
+            <input onChange={inputChangeFirst} className="min-w-50 max-w-100 border-2 border-black mr-4 p-2" type="number" placeholder="Primer Nº" required/>
+            <input onChange={inputChangeSecond} className="min-w-50 max-w-100 border-2 border-black p-2" type="number" placeholder="Segundo Nº" required/>
+        </section>
+
+        <section className="flex gap-3 mb-4">
+            <button onClick={sumar} className="p-2 border-2 border-black hover:cursor-pointer ">+</button>
+            <button onClick={restar} className="p-2 border-2 border-black hover:cursor-pointer ">-</button>
+            <button onClick={multiplicar} className="p-2 border-2 border-black hover:cursor-pointer ">*</button>
+            <button onClick={dividir} className="p-2 border-2 border-black hover:cursor-pointer ">/</button>
+        </section>
+
+        <p>{result}</p>
         </>
+    )
+}
+
+
+export default function Ejercicio5() {
+    const ejercicio5 = `Crea una página con dos campos de entrada de números y cuatro botones: "Sumar", "Restar", "Multiplicar", y "Dividir". 
+    Al hacer clic en cualquiera de los botones, debe mostrarse el resultado de la operación en un área de texto o debajo de los botones. 
+    Asegúrate de validar los datos para evitar errores (como división por cero o entradas vacías).`
+    
+    return (
+        <div className="p-8">
+        <Encabezado texto={ejercicio5} num={5}></Encabezado>
+        <OperacionesMatematicas></OperacionesMatematicas>
+        </div>
     );
 }
--- src/exercises/ejercicio6.jsx
+++ src/exercises/ejercicio6.jsx
@@ -1,7 +1,11 @@
 import Encabezado from "../components/encabezado";
 
 export default function Ejercicio6() {
-    const ejercicio6 = ``
+    const ejercicio6 = `Crea una página con un temporizador que comience en 00:00:00. Incluye tres botones: “Iniciar”, “Pausar” y “Reiniciar”. 
+    Al hacer clic en “Iniciar”, el temporizador debe comenzar a contar los segundos, minutos y horas. 
+    “Pausar” detiene el conteo pero mantiene el tiempo actual. 
+    “Reiniciar” pone el temporizador en 00:00:00`
+    
     return (
         <>
         <Encabezado texto={ejercicio6} num={6}></Encabezado>
