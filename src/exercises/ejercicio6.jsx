2d6ccfb53e5d2e97c83e860d231bc1bbfdddf54d
AnthonyBrito-dev <anthonyrafaelbritobravo10@gmail.com>
2026-04-01 12:37:41 -0500

    Resolucion del ejercicio 6 de react

--- src/exercises/ejercicio6.jsx
+++ src/exercises/ejercicio6.jsx
@@ -1,12 +1,14 @@
 import Encabezado from "../components/encabezado";
-import { useState } from "react";
+import {useState} from "react";
 
 //Crear el formato del temporizador
+let seg = 0
+let min = 0
+let hr = 0
+
 function Temporizador(){
+
     const [tiempo, setTiempo] = useState('00:00:00')
-    let seg = 0
-    let min = 0
-    let hr = 0
     
     //Funcion para dar formato al tiempo
     const shownumbers = (num)=>{
@@ -19,13 +21,21 @@ function Temporizador(){
         }
 
         return numeroFormateado
-    }
+    } 
 
+    const [segund, setSegund] = useState([seg,min,hr])
+    
     //Crear componente iniciar
     const iniciar = ()=>{
-        setInterval(() => {
+    
+        let t = setInterval(() => {
             seg += 1
+            
             setTiempo(`${shownumbers(hr)}:${shownumbers(min)}:${shownumbers(seg)}`)
+            
+            setSegund(segund.splice(0,3, [seg, min, hr]))
+            
+            console.log('Estado de seg:' + segund) 
     
             if (seg == 59){
                 min += 1
@@ -36,21 +46,34 @@ function Temporizador(){
                 min = 0
                 hr += 1
             }
+            
+            const botonParar = document.getElementById("parar")
+            botonParar.addEventListener('click', ()=>{
+                clearInterval(t)
+            })
+
+            const botonReiniciar = document.getElementById("reiniciar")
+            botonReiniciar.addEventListener("click", ()=>{
+                seg = 0
+                min = 0
+                hr = 0
+                clearInterval(t)
+                setTiempo('00:00:00')
+            })
+            
         }, 1000)
     }
+
 
     return (
         <>
         <time className="block mb-4" dateTime="HH:MM:SS">{tiempo}</time>
         <button onClick={iniciar} className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Iniciar</button>
-        <button className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Pausar</button>
-        <button className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Reiniciar</button>
+        <button id="parar" className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Pausar</button>
+        <button id='reiniciar' className="p-2 border-2 border-black rounded-2xl hover:cursor-pointer">Reiniciar</button>
         </>
     )
 }
-//Modificar su estado en cada intervalo de un minuto
-//Crear componente pausar
-//Crear componente reiniciar
 
 export default function Ejercicio6() {
     const ejercicio6 = `Crea una página con un temporizador que comience en 00:00:00. Incluye tres botones: “Iniciar”, “Pausar” y “Reiniciar”. 
